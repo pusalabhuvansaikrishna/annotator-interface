ca1eda84c57e9f6004cfc460d61543bb7093c0b1
Bhuvan Sai Krishna <pusalabhuvansaikrishna@gmail.com>
2026-07-09 11:52:04 +0530

    Updated the paths

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
-/*export const BASE_URL = "http://10.4.16.36:8005";*/
+/*export const BASE_URL = "http://localhost:8000";*/
+export const BASE_URL = "http://10.4.16.36:8005";
 
-export const SPELLCHECK_URL="http://localhost:8001"
+/*export const SPELLCHECK_URL="http://localhost:8001"*/
+export const SPELLCHECK_URL="http://10.4.16.36:8006"
